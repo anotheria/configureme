08f38bdd3bd035ecd18eeb82b23fe35b906fa1d6
dvayanu <dvayanu@02d6fa54-d939-0410-b9b5-aa831fdde1ee>
2009-04-13 21:30:26 +0000

    increased testsuite

---
+++ test/junit/org/configureme/ConfigurableWithPublicFields.java
@@ -0,0 +1,74 @@
+package org.configureme;
+
+import org.configureme.annotations.AfterConfiguration;
+import org.configureme.annotations.AfterInitialConfiguration;
+import org.configureme.annotations.AfterReConfiguration;
+import org.configureme.annotations.BeforeConfiguration;
+import org.configureme.annotations.BeforeInitialConfiguration;
+import org.configureme.annotations.BeforeReConfiguration;
+import org.configureme.annotations.Configure;
+import org.configureme.annotations.ConfigureMe;
+import org.configureme.sources.ConfigurationSourceKey;
+
+@ConfigureMe(name="fixture", type=ConfigurationSourceKey.Type.FIXTURE)
+public class ConfigurableWithPublicFields {
+	@Configure public short shortValue;
+	@Configure public long longValue;
+	@Configure public int intValue;
+	@Configure public boolean booleanValue;
+	@Configure public String stringValue;
+	@Configure public byte byteValue;
+	@Configure public float floatValue;
+	@Configure public double doubleValue ;
+	@Configure public int onlyInA;
+	@Configure public int onlyInB;
+	
+	private boolean beforeConfigCalled, afterConfigCalled, beforeInitialConfigCalled, afterInitialConfigCalled, beforeReConfigCalled, afterReConfigCalled;
+	
+	public String toString(){
+		String ret = "";
+		ret += "short: "+shortValue;
+		ret += " long: "+longValue;
+		ret += " int: "+intValue;
+		ret += " boolean: "+booleanValue;
+		ret += " string: "+stringValue;
+		ret += " byte: "+byteValue;
+		ret += " float: "+floatValue;
+		ret += " double: "+doubleValue;
+		ret += " onlyInA: "+onlyInA;
+		ret += " onlyInB: "+onlyInB;
+		return ret;
+	}
+	
+	public boolean isBeforeConfigCalled() {
+		return beforeConfigCalled;
+	}
+
+	public boolean isAfterConfigCalled() {
+		return afterConfigCalled;
+	}
+
+	public boolean isBeforeInitialConfigCalled() {
+		return beforeInitialConfigCalled;
+	}
+
+	public boolean isAfterInitialConfigCalled() {
+		return afterInitialConfigCalled;
+	}
+
+	public boolean isBeforeReConfigCalled() {
+		return beforeReConfigCalled;
+	}
+
+	public boolean isAfterReConfigCalled() {
+		return afterReConfigCalled;
+	}
+	
+	@BeforeConfiguration public void before(){ beforeConfigCalled = true; }
+	@BeforeInitialConfiguration public void beforeInitial(){ beforeInitialConfigCalled = true; }
+	@BeforeReConfiguration public void beforeRe(){ beforeReConfigCalled = true; }
+	
+	@AfterConfiguration public void after(){ afterConfigCalled = true; }
+	@AfterInitialConfiguration public void afterInitial(){ afterInitialConfigCalled = true; }
+	@AfterReConfiguration public void afterRe(){ afterReConfigCalled = true; }
+}
--- test/junit/org/configureme/ConfigureMeTestSuite.java
+++ test/junit/org/configureme/ConfigureMeTestSuite.java
@@ -8,14 +8,16 @@ import org.configureme.repository.ConfigurationRepositoryTest;
 import org.configureme.sources.ConfigurationSourceKeyTest;
 import org.configureme.sources.ConfigurationSourceRegistryAddTest;
 import org.configureme.sources.ConfigurationSourceRegistryTest;
+import org.configureme.sources.FileLoaderTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(value=Suite.class)
 @SuiteClasses(value={DynamicEnvironmentTest.class, ApplicationEnvironmentTest.class, LocaleBasedEnvironmentTest.class, GlobalEnvironmentTest.class,
-		ConfigurationSourceRegistryAddTest.class, ConfigurationSourceRegistryTest.class, ConfigurationSourceKeyTest.class,
-		AttributeValueTest.class, ConfigurationRepositoryTest.class})
+		ConfigurationSourceRegistryAddTest.class, ConfigurationSourceRegistryTest.class, ConfigurationSourceKeyTest.class, FileLoaderTest.class,
+		AttributeValueTest.class, ConfigurationRepositoryTest.class, 
+		AutoConfigTest.class, TestForErrors.class, ReadConfigurationViaManager.class, AutoReConfig.class})
 public class ConfigureMeTestSuite {
 
 }
